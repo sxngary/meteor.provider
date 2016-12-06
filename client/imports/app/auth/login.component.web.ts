c94c6d9acb4657fbefb2bc9af4f295c7f3581995
garysxn <gauravsa@smartdatainc.net>
2016-12-06 09:53:52 +0530

    login signup page

--- client/imports/app/auth/login.component.web.ts
+++ client/imports/app/auth/login.component.web.ts
@@ -10,31 +10,32 @@ import template from './login.component.web.html';
   template
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  error: string;
-
-  constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
-    this.error = '';
-  }
-
-  login() {
-    if (this.loginForm.valid) {
-      Meteor.loginWithPassword(this.loginForm.value.email, this.loginForm.value.password, (err) => {
-        if (err) {
-          this.zone.run(() => {
-            this.error = err;
-          });
-        } else {
-          this.router.navigate(['/dashboard']);
-        }
-      });
+    loginForm: FormGroup;
+    error: string;
+  
+    constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {}
+  
+    ngOnInit() {
+        var emailRegex = "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})";
+        this.loginForm = this.formBuilder.group({
+          email: ['', Validators.compose([Validators.pattern(emailRegex), Validators.required])],
+          password: ['', Validators.required]
+        });
+    
+        this.error = '';
     }
-  }
+  
+    login() {
+        if (this.loginForm.valid) {
+            Meteor.loginWithPassword(this.loginForm.value.email, this.loginForm.value.password, (err) => {
+                if (err) {
+                    this.zone.run(() => {
+                      this.error = err;
+                    });
+                } else {
+                    this.router.navigate(['/dashboard']);
+                }
+            });
+        }
+    }
 }
--- client/imports/app/auth/singup.component.ts
+++ client/imports/app/auth/singup.component.ts
@@ -15,36 +15,37 @@ export class SignupComponent implements OnInit {
 
   constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
-    this.signupForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required]
-    });
-
-    this.error = '';
-  }
-
-  signup() {
-    if (this.signupForm.valid) {
-      Accounts.createUser({
-        email: this.signupForm.value.email,
-        password: this.signupForm.value.password,
-        type: "provider",
-        profile: {
-          firstName: this.signupForm.value.firstName,
-          lastName: this.signupForm.value.lastName
-        }
-      }, (err) => {
-        if (err) {
-          this.zone.run(() => {
-            this.error = err;
-          });
-        } else {
-          this.router.navigate(['/dashboard']);
-        }
-      });
+    ngOnInit() {
+        var emailRegex = "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})";
+        this.signupForm = this.formBuilder.group({
+          email: ['', Validators.compose([Validators.pattern(emailRegex), Validators.required])],
+          password: ['', Validators.compose([Validators.required, Validators.minLength(6)]) ],
+          firstName: ['', Validators.compose([Validators.required, Validators.pattern("[a-zA-Z ]{2,30}")])],
+          lastName: ['', Validators.compose([Validators.required, Validators.pattern("[a-zA-Z ]{2,30}")])],
+        });
+  
+        this.error = '';
     }
-  }
+
+    signup() {
+        if (this.signupForm.valid) {
+          Accounts.createUser({
+            email: this.signupForm.value.email,
+            password: this.signupForm.value.password,
+            type: "provider",
+            profile: {
+              firstName: this.signupForm.value.firstName,
+              lastName: this.signupForm.value.lastName
+            }
+          }, (err) => {
+            if (err) {
+              this.zone.run(() => {
+                this.error = err;
+              });
+            } else {
+              this.router.navigate(['/dashboard']);
+            }
+          });
+        }
+    }
 }
